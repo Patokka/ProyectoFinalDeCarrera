3be4702062fc4e635d63f1399d4eb92c00880817
Patricio Gaggiotti Bussi <150188000+Patokka@users.noreply.github.com>
2025-11-21 18:46:07 -0300

    Remove allowedDevOrigins from next.config.js
    Commit 7 para eliminar variables sensibles

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  allowedDevOrigins: ['http://192.168.0.6:3000', 'http://192.168.0.11:3000'],
   async rewrites() {
     return [
       {
